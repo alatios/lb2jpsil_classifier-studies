419603511ca8bf1195a716c7f69d9bf42771de62
Alessandro De Gennaro <alessandro.de.gennaro@cern.ch>
2022-01-24 18:19:43 +0100

    Optimize several HBDT models, begin threshold optimization

--- 20_HBDT/22_perform_grid_scan.py
+++ 20_HBDT/22_perform_grid_scan.py
@@ -9,13 +9,37 @@ import time
 #import joblib
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn import ensemble, model_selection
+import pathlib
+
+## For testing, this will take less time and will check if the code has bugs
+#parameters = {
+#    'learning_rate': [0.01, 0.02],
+#	'max_leaf_nodes': [200, 400],
+#	'max_iter'    : [1500, 2500],
+#}
+
+parameters = {
+	'learning_rate': [0.003, 0.006, 0.010, 0.015],
+	'max_leaf_nodes': [100, 200, 400, 800],
+	'max_iter'    : [1500, 2500, 5000],
+}
+
+## Two options:
+## 'balance': same number of training events for signal and bkg
+## 'bkgskew' uses 4 mln events of bkg, 500k of signal
+dataset_type = 'balance'
+
+if dataset_type not in ['balance', 'bkgskew']:
+    raise ValueError
 
 ## Results CSV file
 output_result_directory = './results/'
-output_csv = output_result_directory + 'HBDT_crossval_results.csv'
+output_csv = output_result_directory + 'HBDT_crossval_results_' + dataset_type + '.csv'
 
 ## Load training data
-input_train = '~/classifier-studies/data/trainData.h5'
+repo_dir = str(pathlib.Path.home()) + '/classifier-studies/'
+input_dir = repo_dir + '/data/'
+input_train = input_dir + 'train_dataset_' +  dataset_type + '.h5'
 df_train = pd.read_hdf(input_train, 'LHCb_Train')
 
 ## Perform the training using DTF_FixJPsi momenta.
@@ -43,19 +67,6 @@ features = [
 
 X_train = df_train.loc[:, features].to_numpy()
 Y_train = df_train.loc[:, 'TYPE'].to_numpy()
-
-parameters = {
-	'learning_rate': np.logspace(-2.5, -1.75, 4),
-	'max_leaf_nodes': [200, 400, 800],
-	'max_iter'    : [1500, 2500, 5000],
-}
-
-## For testing, this will take less time and will check if the code has bugs
-#parameters = {
-#    'learning_rate': [0.01, 0.02],
-#	'max_leaf_nodes': [200, 400],
-#	'max_iter'    : [1500, 2500],
-#}
 
 n_jobs = 10
 
--- 20_HBDT/README.md
+++ 20_HBDT/README.md
@@ -1,7 +1,7 @@
 The naming convention for HBDT files is as follows:
 
 ```
-HBDT_{P}_{LR}_{MLN}_{MI}
+HBDT_{P}_{LR}_{MLN}_{MI}_{BAL}
 ```
 
 Legend:
@@ -10,3 +10,4 @@ Either `VF` for Vertex Fitter, `DTF` for DecayTreeFitter *without* &Lambda; mass
 * `LR`: the learning rate (shrinks the contribution of each tree).
 * `MLN`: maximum number of leaves in each tree.
 * `MI`: maximum number of iterations of the boosting process.
+* `BAL`: training set balance, either `BAL` for balanced (same number of signal and bkg events) or `SKEW` for skewed (way more background events).
